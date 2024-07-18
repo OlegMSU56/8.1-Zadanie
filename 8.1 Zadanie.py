28e9937c192a9da5787f72fb5d7172adef61bce1
Oleg <sagdeev.ol@yandex.ru>
2024-07-18 22:26:19 +0500

    Add files via upload

---
+++ 8.1 Zadanie.py
@@ -0,0 +1,16 @@
+def add_everything_up(a, b):
+    try:
+        c = round(a + b, 3)
+    except TypeError as exc:
+        a = str(a)
+        b = str(b)
+        c = a + b
+    return c
+
+
+
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
